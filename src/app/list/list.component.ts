efdadaeee633a263e285e42a7902843828819e36
dislersd <dislersd@gmail.com>
2020-07-01 10:35:43 -0700

    added http service - connected it with list component

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
+  constructor(private _http: HttpService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this._http.myMethod();
   }
-
 }
