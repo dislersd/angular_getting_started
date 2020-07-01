e4bb63a1c9d84dc0c5a8ac8245a47231a2b3c5a0
dislersd <dislersd@gmail.com>
2020-07-01 10:42:58 -0700

    fetching data from api storing breweries in brews object

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; 
 
 @Injectable({
   providedIn: 'root',
 })
 export class HttpService {
-  constructor() {}
+  // dependancy injection 
+  constructor(private http: HttpClient) {}
 
-  myMethod() {
-    return console.log('Hey what is up!');
+  getBeer() {
+    return this.http.get('https://api.openbrewerydb.org/breweries')
   }
 }
